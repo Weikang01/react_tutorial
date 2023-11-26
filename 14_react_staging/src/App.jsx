5a94c27c60aaa9b82be280c381dcf17446ba0966
weikang.liu <weikang.liu@oc.netease.com>
2023-11-26 00:03:18 -0500

    react staging hello world

---
+++ 14_react_staging/src/App.jsx
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+import Index from './components/Hello';
+
+export default class App extends Component {
+    render() {
+        return (
+            <div>
+                <Index/>
+            </div>
+        );
+    }
+}
+
